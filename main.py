ea30c87b61a07e6d70c67e6fdc15342da0802024
panthurr0 <olapopaola@mail.ru>
2024-03-22 16:27:52 +0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-<!doctype html>
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+import time
+
+# Для начала определим настройки запуска
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def __get_html_content(self):
+        return """
+        <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -361,3 +374,26 @@
 </script>
 </body>
 </html>
+        """
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        print(query_components)
+        page_content = self.__get_html_content()
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
